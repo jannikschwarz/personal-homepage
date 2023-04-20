1fd2d97a1ddbdf76c702a3f9ca1ffbff3dd7e570
Jannik Schwarz-Wolf <jannikschwarz@live.dk>
2023-04-20 12:26:00 +0200

    Fixed firestore and Auth

---
+++ src/lib/logins.js
@@ -0,0 +1,14 @@
+export let logginEmails = [
+    "private@mail.com",
+    "volvo@mail.com",
+    "atlasCopco@mail.com",
+    "noaIgnite@mail.com",
+    "seb@mail.com",
+    "edclark@mail.com",
+    "munters@mail.com",
+    "abios@mail.com",
+    "nexer@mail.com",
+    "linkdin@mail.com",
+    "odevo@mail.com",
+    "hiq@mail.com"
+]
